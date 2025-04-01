9aa1b2799da1ffa39a1308fc8494c846030bedbc
Jonathan Pollak <pollak.jonathan@gmail.com>
2025-04-01 15:44:18 -0600

    structure project

--- examples/math-teacher/webpack.config.js
+++ examples/math-teacher/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/agents'),
+    path: join(__dirname, '../../dist/examples/math-teacher'),
   },
   plugins: [
     new NxAppWebpackPlugin({
